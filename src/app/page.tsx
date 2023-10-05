5fd135056ed74b54c7a7bb148543caaeaf73b62d
Aditya <adityasinghboss1234@gmail.com>
2023-10-05 20:27:15 +0530

    feat: landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
-  return <h1 className="text-red-600"> hi</h1>;
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <h1 className="font-semibbold text-5xl text-center">
+        Make Learning easier{" "}
+        <span className="font-bold text-yellow-600">Learn GPT</span> {""}
+        AI powered learning platform
+      </h1>
+      <div className="mt-4"></div>
+      <div className="flex justify-center">
+        <Link href="/gallery">
+          <Button className="bg-yellow-600">
+            Explore Now
+            <ArrowRight className="ml-2 w-5 h-5" strokeWidth={3} />
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
