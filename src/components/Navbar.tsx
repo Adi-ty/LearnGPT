dcb7d0d0b0e79a443956e94d190f9359f95d17a2
Aditya <adityasinghboss1234@gmail.com>
2023-10-05 20:10:06 +0530

    feat: ui changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,9 +14,9 @@ const Navbar = async (props: Props) => {
     <nav className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
       <div className="flex items-center justify-center h-full gap-2 px-8 mx-auto sm:justify-between max-w-7xl">
         <Link href="/" className="items-center hidden gap-2 sm:flex">
-          <span className="font-bold text-xl px-2 py-1 border-b-4 border-black">
+          <p className="font-bold text-xl px-2 py-1 border-b-4 border-black dark:border-white">
             Learn GPT
-          </span>
+          </p>
         </Link>
         <div className="flex items-center gap-3">
           <Link href="/gallery" className="mr-3">
