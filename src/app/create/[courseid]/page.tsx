93442e98fb1a5bda07a25f5c1a03d40a7934e3fb
Aditya <adityasinghboss1234@gmail.com>
2023-10-06 13:03:15 +0530

    feat(ui): Create course page

--- src/app/create/[courseid]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const createChapters = (props: Props) => {
-  return <div>createChapters</div>;
-};
-
-export default createChapters;
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -12,7 +12,7 @@ const CreatePage = async (props: Props) => {
     return redirect("/");
   }
   return (
-    <div className="flex flex-col items-center max-w-xl px-8 mx-auto my-16 sm:px-0">
+    <div className="flex flex-col items-center max-w-xl px-8 mx-auto my-4 sm:px-0">
       <h1 className="self-center text-3xl font-bold text-center sm:text-6xl">
         Learn GPT
       </h1>
